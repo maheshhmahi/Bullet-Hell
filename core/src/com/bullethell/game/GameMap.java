2a975cf4949f74642d5a7048ebfc1f5d26c5099f
priya damodharan <priyasdamodharan@gmail.com>
2024-02-13 20:41:09 -0800

    fixed errors

--- core/src/com/bullethell/game/GameMap.java
+++ core/src/com/bullethell/game/GameMap.java
@@ -8,8 +8,16 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.TimeUtils;
-import com.bullethell.game.characters.enemy.pattern.*;
+import com.bullethell.game.characters.enemy.pattern.EnemyPattern;
 
+import com.bullethell.game.characters.enemy.pattern.FinalBossPattern;
+import com.bullethell.game.characters.enemy.pattern.MidBossAPattern;
+
+import com.bullethell.game.characters.enemy.pattern.FinalBossPattern;
+
+import com.bullethell.game.characters.enemy.pattern.MidBossAPattern;
+import com.bullethell.game.characters.enemy.pattern.MidBossbPattern;
+import com.bullethell.game.characters.enemy.pattern.GeneralEnemyaPattern;
 import com.bullethell.game.characters.hero.Hero;
 
 import java.util.ArrayList;
@@ -66,8 +74,7 @@ public class GameMap extends Map {
         timePeriod(7000, 7020, new MidBossbPattern()); // Adjusted timing for the second MidBossbPattern
         timePeriod(20000, 20020, new MidBossbPattern());
         timePeriod(40000, 40020, new MidBossAPattern());
-        timePeriod(40000, 40020, new FinalBossPattern());
-
+        timePeriod(40000, 40020, new FinalBossPattern()); // Adjusted timing for the FinalBossPattern
 
 
         super.update(delta);
