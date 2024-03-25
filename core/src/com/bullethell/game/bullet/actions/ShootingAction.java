97e122a0675cd0b87005bc63745fff4244956bb9
Mahesh Kumar <mk799994@gmail.com>
2024-03-24 17:17:19 -0700

    REFACTORED: shooting actions

--- core/src/com/bullethell/game/bullet/actions/ShootingAction.java
+++ core/src/com/bullethell/game/bullet/actions/ShootingAction.java
@@ -2,10 +2,13 @@ package com.bullethell.game.bullet.actions;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public interface ShootingAction {
 
     void shoot(float deltaTime, int speed);
 
-    void render(SpriteBatch batch, Texture image);
+    void render(SpriteBatch batch);
+
+    Rectangle getHitBox();
 }
